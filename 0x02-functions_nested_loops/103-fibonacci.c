48263767b5d01d38f5c0f8a1853283f3d5f7f4d6
sarahbaraza254044 <sarahbaraza66@gmail.com>
2022-07-05 16:52:15 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,7 +2,7 @@
 /**
  * main - main block
  * Description: Print the sum of even Fibonacci numbers up to a fib value
- * not exceeding 4,000,000. 
+ * not exceeding 4,000,000.
  * Return: 0
  */
 int main(void)
