8153e90112d9e90b5f50853cb940b7712f2ac0d8
sarahbaraza254044 <sarahbaraza66@gmail.com>
2022-08-22 11:55:44 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,69 +1,160 @@
 #include <elf.h>
+
+
+
 #include <sys/types.h>
+
+
+
 #include <sys/stat.h>
+
+
+
 #include <fcntl.h>
+
+
+
 #include <unistd.h>
+
+
+
 #include <stdio.h>
+
+
+
 #include <stdlib.h>
+
+
+
+
+
+
+
 void check_elf(unsigned char *e_ident);
+
+
+
 void print_magic(unsigned char *e_ident);
+
+
+
 void print_class(unsigned char *e_ident);
+
+
+
 void print_data(unsigned char *e_ident);
+
+
+
 void print_version(unsigned char *e_ident);
+
+
+
 void print_abi(unsigned char *e_ident);
+
+
+
 void print_osabi(unsigned char *e_ident);
+
+
+
 void print_type(unsigned int e_type, unsigned char *e_ident);
+
+
+
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+
+
+
 void close_elf(int elf);
+
+
+
+
+
+
+
 /**
- * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- * Description: If the file is not an ELF file - exit code 98.
- */
+ *
+ *  *
+ *
+ *   *  * check_elf - Checks if a file is an ELF file.
+ *
+ *    *
+ *
+ *     *   * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ *      *
+ *
+ *       *    *
+ *
+ *        *
+ *
+ *         *     * Description: If the file is not an ELF file - exit code 98.
+ *
+ *          *
+ *
+ *           *      */
+
+
+
 void check_elf(unsigned char *e_ident)
+
+
+
 {
-int index;
-for (index = 0; index < 4; index++)
 
 
 
-						{
+			int index;
 
 
 
-															if (e_ident[index] != 127 &&
 
 
 
-																														e_ident[index] != 'E' &&
+
+						for (index = 0; index < 4; index++)
 
 
 
-																																														e_ident[index] != 'L' &&
+												{
 
 
 
-																																																																	e_ident[index] != 'F')
+																					if (e_ident[index] != 127 &&
 
 
 
-																											{
+																																				e_ident[index] != 'E' &&
 
 
 
-																																											dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+																																																				e_ident[index] != 'L' &&
 
 
 
-																																																													exit(98);
+																																																																							e_ident[index] != 'F')
 
 
 
-																																																																																	}
+																																	{
 
 
 
-																								}
+																																																	dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+
+
+
+																																																																			exit(98);
+
+
+
+																																																																																							}
+
+
+
+																														}
 
 
 
