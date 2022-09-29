48fa39a0a5c6c4ec04bc26aa93f14f83c031de51
sarahbaraza254044 <sarahbaraza66@gmail.com>
2022-09-29 11:36:48 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,13 +6,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
-	if (!h)
-		return (0);
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		nodes++;
-	}
-	return (nodes);
+size_t nodes = 0;
+if (!h)
+return (0);
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+nodes++;
+}
+return (nodes);
+}
